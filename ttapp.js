d5c284d4c387c3e7ee22282c3c03467e98b5813c
Michael Traher <matraher1@gmail.com>
2018-07-13 15:25:41 -0400

    Instructions added.

--- ttapp.js
+++ ttapp.js
@@ -1,3 +1,9 @@
+/*I'm supposed to take these and assign the links to each variable.
+I've not been able to even get the navigation links to work yet but
+I'm sure that if I figure out one I'll figure out the rest.
+*/
+
+
 let navigationLinks;
 let galleryLinks;
 let footerImages;
