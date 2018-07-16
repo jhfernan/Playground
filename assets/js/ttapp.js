6bdad4bc8ff9c7053cf2c2714f4ab35c2bdb4ed1
Jonathan Fernandes <jhfernan@mail.usf.edu>
2018-07-16 08:45:00 -0400

    Examples for Mike on how to manipulate the DOM

--- assets/js/ttapp.js
+++ assets/js/ttapp.js
@@ -1,4 +1,5 @@
-/*I'm supposed to take these and assign the links to each variable.
+/*
+I'm supposed to take these and assign the links to each variable.
 I've not been able to even get the navigation links to work yet but
 I'm sure that if I figure out one I'll figure out the rest.
 */
@@ -11,3 +12,16 @@ let footerImages;
 galleryLinks = document.getElementById('gallery');
 
 navigationLinks = document.getElementById('nav links');
+
+console.log(galleryLinks);
+console.log(typeof galleryLinks);
+
+var items = galleryLinks.getElementsByTagName('li');
+console.log(items);
+console.log(typeof items);
+
+console.log('In the list: ', items[0].getElementsByTagName('a')[0].href);
+
+for (var i = 0; i < items.length; i++) {
+	console.log(items[i]);
+};
