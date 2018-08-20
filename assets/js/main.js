c5a229fbabf91304d5032791a9db10d2baec589b
Jonathan Fernandes <jhfernan@mail.usf.edu>
2018-08-20 14:45:58 -0400

    Added simpel example of Vue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,14 @@
 // Our Javascript page
-alert('You are on this page')
+var app = new Vue({
+	el: '#app',
+	data: {
+		message: 'Hello from Mike and Jon',
+		counter: 0,
+		form: {}
+	},
+	methods: {
+		updateMessage () {
+			this.message = this.form.message
+		}
+	}
+})
